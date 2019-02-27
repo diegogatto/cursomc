13d11d5ceeb78506c59dde44c61e8b162bf3a20e
Diego Gatto <diego.gatto@hotmail.com>
2019-02-27 16:39:48 -0300

    Todas classes faltantes - curso mc finalizado
    Novas classes/serviços - curso mc completo!

--- src/main/java/com/diegogatto/cursomc/domain/Cliente.java
+++ src/main/java/com/diegogatto/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.diegogatto.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable {
@@ -35,6 +36,10 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente () { }
 
@@ -125,6 +130,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 }
